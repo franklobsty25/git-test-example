e8a202a9fb6c296509a2a4017a98a680937dbd0e
Frank Kodie <franklobsty25@icloud.com>
2021-02-01 17:25:26 +0000

    Angular Router Basics

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,15 @@
+// Creating and organising all routes in this file
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+// Novigation paths
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,15 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,11 +30,15 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    AppRoutingModule,
     FlexLayoutModule,
     MatListModule,
     MatGridListModule,
