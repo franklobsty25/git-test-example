8bf4f1a0d354fc6280a50f56649de96626db2bfe
Frank Kodie <franklobsty25@icloud.com>
2021-02-25 02:57:33 +0000

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -14,6 +14,7 @@ export class DishdetailComponent implements OnInit {
 
     dish: Dish;
     dishIds: string[];
+    errMess: string; 
     prev: string;
     next: string;
 
@@ -28,7 +29,8 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds()
     .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe((dish) => { this.dish = dish; this.setPrevNext(dish.id); });
+    .subscribe((dish) => { this.dish = dish; this.setPrevNext(dish.id); },
+      errmess => this.errMess = <any>errmess);
   }
 
   setPrevNext(dishId: string) {
